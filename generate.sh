fed2831e64a387f666e2baa87138d36e6aac1932
tak0kada <okokada.takuya+github@gmail.com>
2021-08-03 11:07:40 +0900

    use image tag for ulf file

--- generate.sh
+++ generate.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-img_tag=$(podman image ls | \
+IMG_TAG=$(podman image ls | \
           grep docker.io/unityci/editor | \
           awk '{print $2}' | \
           peco --prompt "Choose the docker image tag you prefer." 2>/dev/null)
-if [ -z $img_tag ]; then
+if [ -z $IMG_TAG ]; then
     echo -e "error: image tag is blank."
     exit 1
 fi
 
-export IMG_NAME=docker.io/unityci/editor:${img_tag}
+export IMG_TAG
+export IMG_NAME=docker.io/unityci/editor:${IMG_TAG}
 
-envsubst '${IMG_NAME}' < Makefile.template > Makefile
-envsubst '${IMG_NAME}' < Dockerfile.template > Dockerfile
+envsubst '${IMG_NAME} ${IMG_TAG}' < Makefile.template > Makefile
+envsubst '${IMG_NAME} ${IMG_TAG}' < Dockerfile.template > Dockerfile
 
 set +euo pipefail
