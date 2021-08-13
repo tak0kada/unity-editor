9ad8304518d8824e3d39b2c8be6683bc8eb226e7
tak0kada <okokada.takuya+github@gmail.com>
2021-08-13 09:34:53 +0900

    fix: invalid file generated due to inconsistent variable name

--- generate.sh
+++ generate.sh
@@ -10,10 +10,10 @@ if [ -z $IMG_TAG ]; then
     exit 1
 fi
 
-export IMG_TAG
 export IMG_NAME=docker.io/unityci/editor:${IMG_TAG}
+export MY_IMG_TAG=unity-editor-${IMG_TAG}
 
-envsubst '${IMG_NAME} ${IMG_TAG}' < Makefile.template > Makefile
-envsubst '${IMG_NAME} ${IMG_TAG}' < Dockerfile.template > Dockerfile
+envsubst '${IMG_NAME} ${MY_IMG_TAG}' < Makefile.template > Makefile
+envsubst '${IMG_NAME} ${MY_IMG_TAG}' < Dockerfile.template > Dockerfile
 
 set +euo pipefail
